913b3f04066c2e7f2ee5dfd6fc6f3263d26f4e16
eneskaya <eneskaya@me.com>
2016-06-14 16:25:35 +0200

    Remove roboto font

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <title>KRATOS Client</title>
     <link rel="stylesheet" href="/css/app.css">
-    <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
